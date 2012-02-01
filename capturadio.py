3b2ee196572fac3d52a5b2249e51e4d37ee5a157
Dirk Ruediger <dirk@niebegeg.net>
2012-02-01 21:10:37 +0100

    Converted capturadio.py logic into a class.

--- capturadio.py
+++ capturadio.py
@@ -8,89 +8,90 @@ import ConfigParser
 import os
 
 
-def format_date(time_value):
-	if (config.has_section('settings') and config.has_option('settings', 'date_pattern')):
-		pattern = config.get('settings', 'date_pattern', '%Y-%m-%d_%H-%M-%S')
-	else:
-		pattern = '%Y-%m-%d_%H-%M-%S'
-
-	if (type(time_value).__name__=='float'):
-		time_value = time.localtime(time_value)
-	elif (type(time_value).__name__=='struct_time'):
-		pass
-	else:
-		raise TypeError('time_value has to be a struct_time or a float. "%s" given.' % time_value)
-	return time.strftime(pattern, time_value)
-
-def capture(stream_url, duration):
-	import tempfile
-	file_name = u"%s/capturadio_%s.mp3" % (tempfile.gettempdir(), os.getpid())
-	file = open(file_name, 'w+b')
-	not_ready = True
-	try:
-		stream = urllib2.urlopen(stream_url);
-		while not_ready:
-			file.write(stream.read(10240));
-			if ((time.time() - start_time) > duration):
-				not_ready = False
-		file.close
-		return file_name
-	except Exception as e:
-		print "Could not complete capturing, because an exception occured.", e
-		os.remove(file_name)
-
 def as_utf8(string):
 	return u'%s' % unicode(string, 'utf-8')
 
 def as_ascii(string):
 	return '%s' % string.decode('ascii', 'ignore')
 
-def add_metadata(file, station_name, show_title, title):
-	from mutagen.mp3 import MP3
-	import mutagen.id3 
-	if (config.has_section(station_name) and config.has_option(station_name, 'name')):
-		station_name = config.get(station_name, 'name', station_name)
+def format_date(time_value):
+	if (config.has_section('settings') and config.has_option('settings', 'date_pattern')):
+		pattern = config.get('settings', 'date_pattern', '%Y-%m-%d_%H-%M-%S')
+	else:
+		pattern = '%Y-%m-%d_%H-%M-%S'
 
-	show_title = as_utf8(show_title)
-	title = as_utf8(title)
-	station_name = as_utf8(station_name)
-	date = time.strftime('%Y', time.localtime(start_time))
-	comment = u'Show: %s\nEpisode: %s\nCopyright: %s %s' % (show_title, title, date, station_name)
+	if (type(time_value).__name__ == 'float'):
+		time_value = time.localtime(time_value)
+	elif (type(time_value).__name__ == 'struct_time'):
+		pass
+	else:
+		raise TypeError('time_value has to be a struct_time or a float. "%s" given.' % time_value)
+	return time.strftime(pattern, time_value)
 
-	audio = MP3(file)
-	# See http://www.id3.org/id3v2.3.0 for details about the ID3 tags
-	audio["TIT2"] = mutagen.id3.TIT2(encoding=2, text=[title])
-	audio["TDRC"] = mutagen.id3.TDRC(encoding=2, text=[format_date(start_time)])
-	audio["TCON"] = mutagen.id3.TCON(encoding=2, text=[u'Podcast'])
-	audio["TALB"] = mutagen.id3.TALB(encoding=2, text=[show_title])
-	audio["TLEN"] = mutagen.id3.TLEN(encoding=2, text=[duration * 1000])
-	audio["TPE1"] = mutagen.id3.TPE1(encoding=2, text=[station_name])
-	audio["TCOP"] = mutagen.id3.TCOP(encoding=2, text=[station_name])
-	audio["COMM"] = mutagen.id3.COMM(encoding=2, text=[comment])
+class CaptuRadio:
+	def capture(self, stream_url, duration):
+		import tempfile
+		file_name = u"%s/capturadio_%s.mp3" % (tempfile.gettempdir(), os.getpid())
+		file = open(file_name, 'w+b')
+		not_ready = True
+		try:
+			stream = urllib2.urlopen(stream_url);
+			while not_ready:
+				file.write(stream.read(10240));
+				if ((time.time() - start_time) > duration):
+					not_ready = False
+			file.close
+			return file_name
+		except Exception as e:
+			print "Could not complete capturing, because an exception occured.", e
+			os.remove(file_name)
 
-	# APIC part taken from http://mamu.backmeister.name/praxis-tipps/pythonmutagen-audiodateien-mit-bildern-versehen/
-	if (config.has_section(station_name) and config.has_option(station_name, 'logo')):
-		logo = config.get(station_name, 'logo')
-		imgdata = urlopen(logo).read()
-		img = mutagen.id3.APIC(3, u'image/jpeg', 3, u'Station logo', imgdata)
-		audio.tags.add(img)
+	def add_metadata(self, file, station_name, show_title, title):
+		print u"add_metadata: file=%s, station_name=%s, show_title=%s, title=%s" % (file, station_name, show_title, title)
+		from mutagen.mp3 import MP3
+		import mutagen.id3
+		if (config.has_section(station_name) and config.has_option(station_name, 'name')):
+			station_name = config.get(station_name, 'name', station_name)
 
-#		audio["APIC"] = APIC(encoding=0, data=['Podcast'])	
-	audio.save()
+		show_title = as_utf8(show_title)
+		title = as_utf8(title)
+		station_name = as_utf8(station_name)
+		date = time.strftime('%Y', time.localtime(start_time))
+		comment = u'Show: %s\nEpisode: %s\nCopyright: %s %s' % (show_title, title, date, station_name)
 
-def store_file(src_file, destination, station_name, artist, title):
-	import shutil, re
-	if (config.has_section(station_name) and config.has_option(station_name, 'name')):
-		station_name = config.get(station_name, 'name', station_name)
+		audio = MP3(file)
+		# See http://www.id3.org/id3v2.3.0 for details about the ID3 tags
+		audio["TIT2"] = mutagen.id3.TIT2(encoding=2, text=[title])
+		audio["TDRC"] = mutagen.id3.TDRC(encoding=2, text=[format_date(start_time)])
+		audio["TCON"] = mutagen.id3.TCON(encoding=2, text=[u'Podcast'])
+		audio["TALB"] = mutagen.id3.TALB(encoding=2, text=[show_title])
+		audio["TLEN"] = mutagen.id3.TLEN(encoding=2, text=[duration * 1000])
+		audio["TPE1"] = mutagen.id3.TPE1(encoding=2, text=[station_name])
+		audio["TCOP"] = mutagen.id3.TCOP(encoding=2, text=[station_name])
+		audio["COMM"] = mutagen.id3.COMM(encoding=2, text=[comment])
 
-	destination = os.path.expanduser(destination)
-	time_string = format_date(time.localtime(start_time))
-	target_file = u"%s/%s/%s/%s_%s.mp3" % (as_ascii(destination), as_ascii(station_name), as_ascii(artist), as_ascii(title), time_string)
-	target_file = re.compile(u'[^\w\d._/ -]').sub('', target_file)
-	if (not os.path.isdir(os.path.dirname(target_file))):
-		os.makedirs(os.path.dirname(target_file))
-	shutil.copy2(src_file, target_file)
-	return target_file
+		# APIC part taken from http://mamu.backmeister.name/praxis-tipps/pythonmutagen-audiodateien-mit-bildern-versehen/
+		if (config.has_section(station_name) and config.has_option(station_name, 'logo')):
+			logo = config.get(station_name, 'logo')
+			imgdata = urlopen(logo).read()
+			img = mutagen.id3.APIC(3, u'image/jpeg', 3, u'Station logo', imgdata)
+			audio.tags.add(img)
+
+		audio.save()
+
+	def store_file(self, src_file, destination, station_name, artist, title):
+		import shutil, re
+		if (config.has_section(station_name) and config.has_option(station_name, 'name')):
+			station_name = config.get(station_name, 'name', station_name)
+
+		destination = os.path.expanduser(destination)
+		time_string = format_date(time.localtime(start_time))
+		target_file = u"%s/%s/%s/%s_%s.mp3" % (as_ascii(destination), as_ascii(station_name), as_ascii(artist), as_ascii(title), time_string)
+		target_file = re.compile(u'[^\w\d._/ -]').sub('', target_file)
+		if (not os.path.isdir(os.path.dirname(target_file))):
+			os.makedirs(os.path.dirname(target_file))
+		shutil.copy2(src_file, target_file)
+		return target_file
 
 config = ConfigParser.ConfigParser()
 config.read([os.path.expanduser('~/.capturadio/capturadiorc'), os.path.expanduser('~/.capturadiorc')])
@@ -126,7 +127,8 @@ else:
 
 start_time = time.time()
 
-file = capture(config.get('stations', station), duration)
+recorder = CaptuRadio()
 
-file = store_file(file, destination, station, args.b, title)
-add_metadata(file, station, args.b, title)
+file = recorder.capture(config.get('stations', station), duration)
+file = recorder.store_file(file, destination, station, args.b, title)
+recorder.add_metadata(file, station, args.b, title)
