95b487d0fdc5c67f2185b06421d2b21615479587
Dirk Ruediger <dirk@niebegeg.net>
2012-04-02 21:07:43 +0200

    Tests extended and re-structured.

---
+++ tests/suite.py
@@ -0,0 +1,27 @@
+import sys, os, os.path
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from test_config import ConfigurationTestCase
+from test_createrssfeed import ExcludedFoldersTestCase
+
+def config_test_suite():
+    loader = unittest.TestLoader()
+    return loader.loadTestsFromTestCase(ConfigurationTestCase)
+
+def rssfeed_test_suite():
+    loader = unittest.TestLoader()
+    return loader.loadTestsFromTestCase(ExcludedFoldersTestCase)
+
+def test_main():
+    testsuite = config_test_suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
+    result = runner.run(testsuite)
+
+    testsuite = rssfeed_test_suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
+    result = runner.run(testsuite)
+
+if __name__ == "__main__":
+    test_main()
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-import unittest
-import os, sys
+import unittest, os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from capturadio import Configuration, Station, Show
 
 class ConfigurationTestCase(unittest.TestCase):
@@ -35,13 +36,13 @@ class ConfigurationTestCase(unittest.TestCase):
         self.assertEqual(config.stations['dlf'].link_url, 'http://example.org/dlf')
 
         self.assertTrue('dkultur' in config.stations.keys())
-        self.assertEqual(config.stations['dkultur'].stream_url,'http://example.org/dkultur')
+        self.assertEqual(config.stations['dkultur'].stream_url, 'http://example.org/dkultur')
         self.assertEqual(config.stations['dkultur'].name, 'dkultur')
         self.assertEqual(config.stations['dkultur'].logo_url, 'http://example.org/default.png')
         self.assertEqual(config.stations['dkultur'].link_url, 'http://my.example.org/')
 
         self.assertTrue('wdr2' in config.stations.keys())
-        self.assertEqual(config.stations['wdr2'].stream_url,'http://example.org/wdr2')
+        self.assertEqual(config.stations['wdr2'].stream_url, 'http://example.org/wdr2')
         self.assertEqual(config.stations['wdr2'].name, 'wdr2')
         self.assertEqual(config.stations['wdr2'].logo_url, 'http://example.org/wdr2.png')
         self.assertEqual(config.stations['wdr2'].link_url, 'http://example.org/wdr2')
@@ -50,9 +51,9 @@ class ConfigurationTestCase(unittest.TestCase):
         for show_id, show in config.shows.items():
             self.assertTrue(isinstance(show, Show))
 
-        self.assertTrue('dlf_nachtradio' in config.shows.keys())
+        self.assertTrue('nachtradio' in config.shows.keys())
 
-        show = config.shows['dlf_nachtradio']
+        show = config.shows['nachtradio']
         self.assertEqual(show.logo_url, 'http://example.org/nachtradio.png')
         self.assertEqual(show.link_url, 'http://example.org/nachtradio')
         self.assertEqual(show.duration, 3300)
@@ -63,28 +64,49 @@ class ConfigurationTestCase(unittest.TestCase):
         show = config.add_show(station, 'news', 'Latest News', 10)
         self.assertTrue(isinstance(show, Show))
 
-        self.assertEqual(len(config.shows), 4)
+        self.assertEqual(len(config.shows), 3)
         for show_id, show in config.shows.items():
             self.assertTrue(isinstance(show, Show))
-        self.assertEqual(config.shows['dlf_news'].name, 'Latest News')
-        self.assertEqual(config.shows['dlf_news'].logo_url, None)
-        self.assertEqual(config.shows['dlf_news'].station, station)
-        self.assertEqual(config.shows['dlf_news'].duration, 10)
+        self.assertEqual(config.shows['news'].name, 'Latest News')
+        self.assertEqual(config.shows['news'].logo_url, None)
+        self.assertEqual(config.shows['news'].station, station)
+        self.assertEqual(config.shows['news'].duration, 10)
 
     def testParseDuration(self):
         from capturadio import parse_duration
-        self.assertEqual(parse_duration("10h"),     36000)
-        self.assertEqual(parse_duration("50m"),     3000)
-        self.assertEqual(parse_duration("300s"),    300)
-        self.assertEqual(parse_duration("300"),     300)
+
+        self.assertEqual(parse_duration("10h"), 36000)
+        self.assertEqual(parse_duration("50m"), 3000)
+        self.assertEqual(parse_duration("300s"), 300)
+        self.assertEqual(parse_duration("300"), 300)
         self.assertEqual(parse_duration("1h15m20"), 4520)
 
-        self.assertEqual(parse_duration("-50m"),     0)
-        self.assertEqual(parse_duration("-300s"),    0)
-        self.assertEqual(parse_duration("-300"),     0)
+        self.assertEqual(parse_duration("-50m"), 0)
+        self.assertEqual(parse_duration("-300s"), 0)
+        self.assertEqual(parse_duration("-300"), 0)
         self.assertEqual(parse_duration("1h-15m20"), 3600)
-        self.assertEqual(parse_duration("trara"),    0)
-        self.assertEqual(parse_duration("12trara"),  12)
+        self.assertEqual(parse_duration("trara"), 0)
+        self.assertEqual(parse_duration("12trara"), 12)
 
-if __name__ == "__main__":
-    unittest.main()
+
+class ExcludedFoldersTest(unittest.TestCase):
+
+    def testExcludedFolders(self):
+        folders_and_results = {
+            '/var/.git': True,
+            '/var/.git/tra/ra': True,
+            '/var/tmp/.hg': True,
+            '/var/tmp/.hg/git': True,
+            '/var/tmp/.bzr': True,
+            '/var/tmp/.bzr/git/tra': True,
+            '/var/git': False,
+            '/var/git/tra/ra': False,
+            }
+
+        from create_podcast_feed import excluded_folder
+
+        for (folder, result) in folders_and_results:
+            self.assertEqual(excluded_folder(folder), result)
+
+    if __name__ == "__main__":
+        unittest.main()
