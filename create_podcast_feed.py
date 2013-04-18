bdd2ab0a3c5462ca7b4d94606d0e679b5ee295d7
Dirk Rüdiger <dirk@niebegeg.net>
2013-04-18 06:26:46 +0200

    Include the latest 20 episodes into a feed.
    Increased from 10 episodes.

--- create_podcast_feed.py
+++ create_podcast_feed.py
@@ -164,8 +164,8 @@ class Audiofiles:
         self.log.debug(u'  rssitems: Found %d items' % len(result))
         return result
 
-    def getrss(self):
-        items = self.rssitems()
+    def getrss(self, n=10):
+        items = self.rssitems(n)
         config = Configuration()
 
         image = PyRSS2Gen.Image(
@@ -235,7 +235,7 @@ def process_folder(path, root_path):
 
     rss_file = config.feed['file_name']
 
-    rss = audio_files.getrss()
+    rss = audio_files.getrss(20)
     if len(rss.items) > 0:
         rss.write_xml(open(os.path.join(path, rss_file), "w"))
 
