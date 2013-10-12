c01b3381d0c8f5ac934f650efa5dcd8e12ea6fd6
Dirk Rüdiger <dirk@niebegeg.net>
2013-10-12 17:09:54 +0200

    Fixtures refacturized to own package.

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,73 +9,11 @@ as they are greated and managed with the Configuration class.
 
 import os, sys
 import pytest
-from pprint import pprint, pformat
+from fixtures import test_folder
 
 sys.path.insert(0, os.path.abspath('.'))
 
 from capturadio import Configuration, Station, Show
-
-@pytest.fixture
-def test_folder(request, tmpdir):
-  olddir = tmpdir.chdir()
-  request.addfinalizer(olddir.chdir)
-  tmpdir.join('capturadiorc').write('''
-[settings]
-destination = {0}/demodata
-date_pattern = %d.%m.%Y
-comment_pattern:  Show: %show
-  Date: %date
-  Website: %link_url
-  Copyright: %year %station
-
-[stations]
-dlf = http://example.org/dlf
-dkultur = http://example.org/dkultur
-wdr2 = http://example.org/wdr2
-
-[feed]
-url = http://my.example.org
-title = Internet Radio Recordings
-about_url = http://my.example.org/about.html
-description = Recordings
-language = en
-filename = rss.xml
-default_logo_url = http://example.org/default.png
-default_logo_copyright = A Creative Commons license
-
-[dlf]
-name = Deutschlandfunk
-link_url = http://example.org/dlf
-logo_url = http://example.org/dlf.png
-date_pattern = %d.%m.%Y %H:%M
-
-[nachtradio]
-title = Nachtradio
-duration = 3300
-link_url = http://example.org/nachtradio
-logo_url = http://example.org/nachtradio.png
-station = dlf
-
-[weather]
-title = Weather forecast
-duration = 300
-logo_url = http://example.org/weather.png
-station = dlf
-
-[wdr2]
-title = WDR 2
-link_url = http://example.org/wdr2
-logo_url = http://example.org/wdr2.png
-
-[news]
-title = Latest news
-duration = 300
-link_url = http://example.org/wdr2/news
-logo_url = http://example.org/wdr2/news.png
-station = wdr2
-date_pattern = %Y-%m-%d
-'''.format(str(tmpdir)))
-  return tmpdir
 
 def test_configuration(test_folder):
   config = Configuration(
