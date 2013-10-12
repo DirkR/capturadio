99f5d135e488ecf4168ff61ec14f1e75d18d085d
Dirk Rüdiger <dirk@niebegeg.net>
2013-10-12 12:37:21 +0200

    CLI argument parsing converted from argparse to docopt.

--- recorder.py
+++ recorder.py
@@ -1,72 +1,86 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf_8 -*-
+"""capturadio - Capture internet radio programs broadcasted in mp3 encoding format.
 
-import argparse
+Usage:
+  recorder.py [-d destination] [-C configfile] -l length -s station -b title [-t title]
+  recorder.py [-d destination] [-C configfile] -S show [-l length] [-t title]
+
+Options:
+  -h, --help                       show this screen and exit
+  -d folder, --destination folder  Destination directory
+  -C file, --config file           Configuration file
+
+  -l duration, --length duration   Length of recording in seconds
+  -s station, --station station    Name of the station, defined in ~/.capturadio/capturadiorc.
+  -b title, --broadcast title      Title of the show
+  -t title, --title title          Title of the episode
+
+  -S show, --show show             ID of the show, has to be defined in configuration file"""
+
 import sys, os
-from capturadio import Configuration, Station, Show, Recorder
+from docopt import docopt
+from capturadio import Configuration, Station, Show, Recorder, version_string
 from capturadio.util import parse_duration
 
 if __name__ == "__main__":
-
+    args = docopt(__doc__, version=version_string)
+    print(args)
+    #sys.exit(0)
 
     if len(sys.argv) == 1:
         sys.argv.append('--help')
 
-    parser = argparse.ArgumentParser(
-        description='Capture internet radio programs broadcasted in mp3 encoding format.',
-    )
-    parser.add_argument('-d', metavar='destination', required=False, help='Destination directory')
-    parser.add_argument('-C', metavar='configfile', required=False, help='Configuration file')
+    if args['--config'] is not None:
+        if not os.path.exists(os.path.expanduser(args['--config'])):
+            print('Configuration file "%s" does not exist' % args['--config'])
+            sys.exit(1)
+        Configuration.configuration_folder = os.path.dirname(os.path.expanduser(args['--config']))
+        Configuration.filename = os.path.basename(os.path.expanduser(args['--config']))
 
-    detailled_group = parser.add_argument_group()
-    detailled_group.add_argument('-l', metavar='length', required=False, help='Length of recording in seconds')
-    detailled_group.add_argument('-s', metavar='station', required=False, help='Name of the station, defined in ~/.capturadio/capturadiorc.')
-    detailled_group.add_argument('-b', metavar='broadcast', required=False, help='Title of the broadcast')
-    detailled_group.add_argument('-t', metavar='title', required=False, help='Title of the recording')
+    try:
+      config = Configuration()
+    except IOError, e:
+      print('Configuration could not be loaded: %s' % e.reason)
+      sys.exit(1)
 
-    show_group = parser.add_argument_group()
-    show_group.add_argument('-S', metavar='show', required=False, help='ID of the show, has to  be defined in configuration file')
+    if len(config.stations) == 0:
+      print('No stations defined, add stations at first!')
+      sys.exit(0)
 
-    args = parser.parse_args()
-
-    if args.C is not None:
-        if not os.path.exists(os.path.expanduser(args.C)):
-            raise IOError('Configuration file "%s" does not exist' % args.C)
-        Configuration.configuration_folder = os.path.dirname(os.path.expanduser(args.C))
-        Configuration.filename = os.path.basename(os.path.expanduser(args.C))
-
-    config = Configuration()
-
-    if args.S is not None:
+    if args['--show'] is not None:
         show_ids = map(lambda id: id.encode('ascii'), config.shows.keys())
-        if args.S not in config.shows.keys():
-            print "Show '%s' is unknown. Use one of these: %s." % (args.S, show_ids)
+        if args['--show'] not in config.shows.keys():
+            print "Show '%s' is unknown. Use one of these: %s." % (args['--show'], ', '.join(show_ids))
             exit(1)
-        show = config.shows[args.S]
-        if args.t is not None:
-            show.name = u'%s' % unicode(args.t, 'utf8')
-        if args.l is not None:
-            duration = parse_duration(args.l)
+        show = config.shows[args['--show']]
+        if args['--title'] is not None:
+            show.name = u'%s' % unicode(args['--title'], 'utf8')
+        if args['--length'] is not None:
+            duration = parse_duration(args['--length'])
             if duration < 1:
                 print "Length of '%d' is not a valid recording duration. Use a value greater 1." % duration
                 exit(1)
             show.duration = duration
     else:
-        duration = parse_duration(args.l)
+        duration = parse_duration(args['--length'])
         if duration < 1:
             print "Length of '%d' is not a valid recording duration. Use a value greater 1." % duration
             exit(1)
 
-        if args.d is not None:
-            config.set_destination(os.path.expanduser(args.d))
+        if args['--destination'] is not None:
+            config.set_destination(os.path.expanduser(args['--destination']))
 
-        if args.s not in config.get_station_ids():
-            print "Station '%s' is unknown. Use one of these: %s." % (args.s, config.get_station_ids())
+        if args['--station'] not in config.get_station_ids():
+            print "Station '%s' is unknown. Use one of these: %s." % (args['--station'], ', '.join(config.get_station_ids()))
             exit(1)
-        station = config.stations[str.lower(args.s)]
+        station = config.stations[str.lower(args['--station'])]
 
-        title = u'%s' % unicode(args.t if (args.t is not None) else args.b, 'utf8')
+        title = u'%s' % unicode(args['--title'] if (args['--title'] is not None) else args['--broadcast'], 'utf8')
         show = config.add_show(station, title, title, duration)
 
-    recorder = Recorder()
-    recorder.capture(show)
+    try:
+      recorder = Recorder()
+      recorder.capture(show)
+    except Exception, e:
+      print('Unable to capture recording: %s' % e.reason)
